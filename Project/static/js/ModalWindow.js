1d355d6716e66b00c78b3ee36f46185d9340c3b2
Vi1704ca <vica.ti17@gmail.com>
2025-11-30 22:50:28 +0200

    fixed bugs and start make in css modal window

--- Project/static/js/ModalWindow.js
+++ Project/static/js/ModalWindow.js
@@ -2,24 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const openBtn = document.getElementById("open-cart");
     const modal = document.getElementById("cart-modal");
     const frame = document.getElementById("cart-frame");
-    const overlay = document.getElementById("overlay");
 
     openBtn.addEventListener("click", () => {
         if (modal.classList.contains("active")) {
             modal.classList.remove("active");
-            overlay.classList.remove("show");
             frame.src = "";
-            return;
+        } else {
+            frame.src = "/cart";
+            modal.classList.add("active");
         }
-
-        frame.src = "/cart";
-        modal.classList.add("active");
-        overlay.classList.add("show");
-    });
-
-    overlay.addEventListener("click", () => {
-        modal.classList.remove("active");
-        overlay.classList.remove("show");
-        frame.src = "";
     });
 });
