e59d9b301ad191a62a431861cbbdc4eb9f758c12
Vi1704ca <vica.ti17@gmail.com>
2025-12-06 20:15:39 +0200

    update modal window and data bese

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,7 @@ class Product(DATABASE.Model):
     
     product_name = DATABASE.Column(DATABASE.String(50), nullable = False)
     price = DATABASE.Column(DATABASE.Float, nullable = False)
-    #discount = DATABASE.Column(DATABASE.Float, nullable = False)
-    #count = DATABASE.Column(DATABASE.Float, nullable = False)
+    discount = DATABASE.Column(DATABASE.Float, nullable = True)
+    count = DATABASE.Column(DATABASE.Float, nullable = False)
     #description = DATABASE.Column(DATABASE.String(450), nullable = False)
     type_product = DATABASE.Column(DATABASE.String(50), default = 'type')
